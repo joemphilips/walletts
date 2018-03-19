c1a95f5347df76485c742d2da8cb5fea37224108
joemphilips <joemphilips@gmail.com>
2018-03-19 14:02:16 +0900

    tweek test/index.ts

--- packages/walletts/src/test/index.ts
+++ packages/walletts/src/test/index.ts
@@ -1,24 +1,24 @@
 import anyTest, { ExecutionContext, TestInterface } from 'ava';
-import WalletService from '../bin/service';
+import WalletLauncher from '../bin/service';
 import { WalletServiceOpts } from '../lib/config';
 import { AwilixResolutionError } from 'awilix';
 const path = require('path');
 
-type testWalletServiceContext = {
-  service: WalletService;
+type testWalletLauncherContext = {
+  service: WalletLauncher;
 };
-const test = anyTest as TestInterface<testWalletServiceContext>;
-let service: WalletService;
+const test = anyTest as TestInterface<testWalletLauncherContext>;
+let service: WalletLauncher;
 
-test.before((t: ExecutionContext<testWalletServiceContext>) => {
+test.before((t: ExecutionContext<testWalletLauncherContext>) => {
   let opts: WalletServiceOpts = {
     datadir: path.join(__dirname, 'tmp'),
     debugFile: './tmp/debug.log',
     conf: './fixtures/test.conf'
   };
-  service = new WalletService(opts);
+  service = new WalletLauncher(opts);
 });
 
-test.only('wallet service', async t => {
+test.only('wallet service has been started', async t => {
   t.truthy(service);
 });
