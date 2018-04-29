b694635d69be56ee79e00e123c54070920d61c9a
joemphilips <joemphilips@gmail.com>
2018-04-29 16:35:03 +0900

    move bitcoind driver to separate file

---
+++ packages/blockchain-driver/src/lib/trusted-bitcoind.ts
@@ -0,0 +1,44 @@
+/* tslint:disable:no-submodule-imports */
+/* tslint:disable:no-empty */
+import { adapt } from '@cycle/run/lib/adapt';
+import Client, { BatchOption, ClientConstructorOption } from 'bitcoin-core';
+import xs, { MemoryStream, Stream } from 'xstream';
+
+export interface BlockchainAgentOptionBase {
+  readonly url: string;
+}
+
+export type BitcoindRPCRequest = BatchOption;
+
+export interface BitcoindAgentOption extends BlockchainAgentOptionBase {
+  readonly port: number;
+  readonly password: string;
+}
+
+// TODO: Specify type for sink correctly
+export const makeTrustedBitcoindDriver = (
+  clientConstructorOpt?: ClientConstructorOption
+) => {
+  const trustedBitcoindDriver = (
+    request$: Stream<BitcoindRPCRequest>
+  ): MemoryStream<any> => {
+    const client = new Client(clientConstructorOpt ? clientConstructorOpt : {});
+    const response$$ = xs.create();
+
+    // TODO: buffer stream and send request with real batch
+    request$.subscribe({
+      next: async outgoing => {
+        /* tslint:disable-next-line:no-expression-statement */
+        const response$ = xs.fromPromise(client.command([outgoing]));
+        /* tslint:disable-next-line:no-expression-statement */
+        response$.map(response$$.shamefullySendNext);
+      },
+      error: () => {},
+      complete: () => {}
+    });
+
+    return adapt(response$$);
+  };
+
+  return trustedBitcoindDriver;
+};
