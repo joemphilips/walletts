5c6a13851ba985bd74c13937decf0a0f350c6428
joemphilips <joemphilips@gmail.com>
2018-05-11 20:25:43 +0900

    delete coin-manager and avoid compile error

--- packages/walletts-core/src/index.ts
+++ packages/walletts-core/src/index.ts
@@ -1,12 +1,4 @@
-export * from './lib/coin-manager';
 export * from './lib/config';
-export * from './lib/container';
 export * from './lib/errors';
 export * from './lib/key-repository';
 export * from './lib/logger';
-export * from './bin/grpc-server';
-export * from './lib/stream';
-export * from './bin/uiproxy';
-export * from './lib/wallet';
-export * from './lib/wallet-service';
-export { PROTO_PATH } from './bin/grpc-common';
