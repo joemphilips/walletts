a6c7d192405448a7aa8542b486aabcecdc4069ee
joemphilips <joemphilips@gmail.com>
2018-05-11 16:23:16 +0900

    delete many stuffs

--- packages/walletts-core/src/lib/stream.ts
+++
@@ -1,6 +0,0 @@
-import { Readable, Writable } from 'stream';
-
-// stream for encrypting/decrypting WalletService data
-export class EncryptStream extends Writable {}
-
-export class DecryptStream extends Readable {}
