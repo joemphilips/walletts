162f61fc5190117f50a1d5ae577cbd467a954f41
joemphilips <joemphilips@gmail.com>
2018-05-22 23:50:17 +0900

    implement ThemeDriver in RendererProcess

--- packages/walletts-cycle-electron/app/renderer/drivers/ThemeDriver.ts
+++ packages/walletts-cycle-electron/app/renderer/drivers/ThemeDriver.ts
@@ -1,1 +1,22 @@
-export const createThemeDriver = () => {};
+import { adapt } from "@cycle/run/lib/adapt";
+import { ThemeConfig } from "../interfaces";
+import xs, { MemoryStream, Stream } from "xstream";
+import fromEvent from "xstream/extra/fromEvent";
+import { defaultTheme } from "../styles";
+import { ipcRenderer } from "electron";
+import * as util from "util";
+
+export const createThemeDriver = () => {
+  const ThemeDriver = (
+    themeName$: Stream<string>
+  ): MemoryStream<ThemeConfig> => {
+    const init$ = xs.of(defaultTheme);
+    themeName$.map(name => ipcRenderer.send("loadFile", name));
+    const response$ = fromEvent(ipcRenderer, "theme-loaded") as Stream<
+      ThemeConfig
+    >;
+    return adapt(xs.merge(init$, response$));
+  };
+
+  return ThemeDriver;
+};
