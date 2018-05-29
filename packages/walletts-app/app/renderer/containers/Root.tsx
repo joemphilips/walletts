4b2353154608c8d68244ce8bf7033701862dca40
joemphilips <joemphilips@gmail.com>
2018-05-29 11:44:47 +0900

    delete more useless files

--- packages/walletts-app/app/renderer/containers/Root.tsx
+++ packages/walletts-app/app/renderer/containers/Root.tsx
@@ -1,17 +1,17 @@
-import * as React from 'react';
-import * as Redux from 'react-redux';
-import { History } from 'history';
+import * as React from "react";
+import * as Redux from "react-redux";
+import { History } from "history";
 
-import { Provider } from 'react-redux';
-import { ConnectedRouter } from 'react-router-redux';
-import Routes from '../routes';
+import { Provider } from "react-redux";
+import { ConnectedRouter } from "react-router-redux";
+import Routes from "../routes";
 
 interface IRootType {
   store: Redux.Store<any>;
-  history: History
-};
+  history: History;
+}
 
-export default function Root({ store, history }: IRootType) {
+export function Root({ store, history }: IRootType) {
   return (
     <Provider store={store}>
       <ConnectedRouter history={history}>
