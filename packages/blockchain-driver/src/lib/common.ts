1fe2242d35c1121c9d8bd1dd6f5177deda7297dc
joemphilips <joemphilips@gmail.com>
2018-06-02 18:26:15 +0900

    update typings for blockchain-driver

--- packages/blockchain-driver/src/lib/common.ts
+++ packages/blockchain-driver/src/lib/common.ts
@@ -1,3 +1,8 @@
+import { MemoryStream } from "xstream";
+
 export interface BlockchainAgentOptionBase {
   readonly url: string;
 }
+
+// TODO: add typings
+export type BlockchainSource = MemoryStream<any>
