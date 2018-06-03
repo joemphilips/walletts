4e7b14e00bdd6ba8390c098069ad3af45e026568
joemphilips <joemphilips@gmail.com>
2018-06-04 01:56:08 +0900

    seperate Account container and component

--- packages/walletts-app/app/renderer/components/Account.tsx
+++ packages/walletts-app/app/renderer/components/Account.tsx
@@ -1,8 +1,23 @@
 import * as React from "react";
-import { AccountUIData } from "walletts-components";
+import { IAccountState } from "../store/account/store";
 
-export class Account extends React.Component<AccountUIData> {
+export interface Props extends IAccountState {
+  readonly match: any;
+  readonly location: any;
+  readonly history: any;
+}
+
+export class AccountPage extends React.Component<Props> {
+  constructor(props: Props, context: any) {
+    super(props, context);
+  }
   render() {
-    return <h1> this is account page </h1>;
+    const { id } = this.props.match.params;
+    const { owners } = this.props.accounts[id];
+    if (Object.keys(owners).length <= 1) {
+      return <div> showing account info {id} </div>;
+    } else {
+      return <div> showing community account info {id} </div>;
+    }
   }
 }
--- packages/walletts-app/app/renderer/containers/AccountPage.tsx
+++ packages/walletts-app/app/renderer/containers/AccountPage.tsx
@@ -1,22 +1,17 @@
-import * as React from "react";
-import { AccountUIData } from "walletts-components";
 import { withRouter } from "react-router";
+import {
+  AccountPage as AccountPageComponent,
+  Props
+} from "../components/Account";
+import { IState } from "../store";
+import { connect } from "react-redux";
 
-export interface Props extends AccountUIData {
-  readonly match: any;
-  readonly location: any;
-  readonly history: any;
+function mapStateToProps(state: IState): Partial<Props> {
+  return {
+    accounts: state.accounts.accounts
+  };
 }
 
-export class AccountPage extends React.Component<Props> {
-  constructor(props: Props, context: any) {
-    super(props, context);
-  }
-  render() {
-    const { id } = this.props.match.params;
-    console.log(`going to render account of ${id}`);
-    return <div> showing account info {id} </div>;
-  }
-}
-
-export default withRouter(AccountPage as any);
+export const AccountPage = withRouter(connect(mapStateToProps)(
+  AccountPageComponent
+) as any);
