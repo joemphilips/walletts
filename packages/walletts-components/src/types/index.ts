7c824e86adc3ae28f5f9f1a9e2d3f2de6dd34701
joemphilips <joemphilips@gmail.com>
2018-06-06 18:57:24 +0900

    stop using deprecated react-modal typings

--- packages/walletts-components/src/types/index.ts
+++ packages/walletts-components/src/types/index.ts
@@ -1,3 +1,8 @@
 declare module 'camel-case' {
   export default function camelCase(arg): any;
 }
+
+declare module 'react-modal' {
+  // tslint:disable:readonly-array
+  export default function Modal(...args: any[]): any;
+}
