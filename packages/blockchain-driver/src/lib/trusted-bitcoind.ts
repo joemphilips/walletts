40051f88c88ecaac1b9cab954da45ba6d5d3067b
joemphilips <joemphilips@gmail.com>
2018-06-07 18:18:32 +0900

    implement zmq (without test)

--- packages/blockchain-driver/src/lib/trusted-bitcoind.ts
+++ packages/blockchain-driver/src/lib/trusted-bitcoind.ts
@@ -2,8 +2,9 @@ import { Driver } from '@cycle/run';
 /* tslint:disable:no-submodule-imports */
 import { adapt } from '@cycle/run/lib/adapt';
 import Client, { ClientConstructorOption } from 'bitcoin-core';
-import { Stream } from 'xstream';
+import xs, { Stream } from 'xstream';
 import flattenConcurrently from 'xstream/extra/flattenConcurrently';
+import { socket } from 'zeromq';
 import { requestInputToResponse$ } from './common';
 import {
   BitcoindResponse,
@@ -15,24 +16,57 @@ export interface BitcoindRPCRequest {
   readonly method: keyof Client;
   readonly options?: any;
 }
-
-export interface BitcoindAgentOption extends BlockchainAgentOptionBase {
+export interface ZeromqOption extends BlockchainAgentOptionBase {
   readonly port: number;
-  readonly password: string;
+}
+
+export function createZmqStream(
+  option?: ZeromqOption | boolean,
+  url?: string
+): Stream<BitcoindResponse> {
+  const normalizedURL = url ? url : '127.0.0.1';
+  const normalizedPort = !option
+    ? '28332'
+    : typeof option === 'boolean'
+      ? '28332'
+      : option.port;
+  const host = 'tcp://' + normalizedURL + ':' + normalizedPort;
+  const sock = socket(host);
+  sock.subscribe('rawblock');
+  sock.subscribe('rawtransaction');
+  const stream = xs.create<BitcoindResponse>({
+    start: listener => {
+      sock.on('message', (topic, message) => {
+        listener.next({
+          nodeType: SupportedBchType.BITCOIN_CORE,
+          type: topic.toString() === 'zmqblock' ? 'zmqblock' : 'zmqtx',
+          result: message,
+          meta: {}
+        });
+      });
+    },
+    // tslint:disable-next-line
+    stop: () => {}
+  });
+  return stream;
 }
 
 export const makeTrustedBitcoindDriver = (
-  clientConstructorOpt?: ClientConstructorOption
+  clientConstructorOpt?: ClientConstructorOption,
+  zmq?: ZeromqOption | boolean
 ): Driver<Stream<BitcoindRPCRequest>, Stream<BitcoindResponse>> => {
   const trustedBitcoindDriver = (
     request$: Stream<BitcoindRPCRequest>
   ): Stream<BitcoindResponse> => {
     const cli = new Client(clientConstructorOpt);
 
-    // TODO: buffer stream and send request with real batch
-    const response$ = request$
+    const rpcResponse$ = request$
       .map(x => requestInputToResponse$(cli, x, SupportedBchType.BITCOIN_CORE))
       .compose(flattenConcurrently);
+
+    const response$ = zmq
+      ? xs.merge(rpcResponse$, createZmqStream(zmq, clientConstructorOpt.host))
+      : rpcResponse$;
 
     return adapt(response$);
   };
