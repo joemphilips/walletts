76f8528d295583fcb7e36f475cce6302fd4f8bf1
joemphilips <joemphilips@gmail.com>
2018-06-09 20:58:52 +0900

    install yarn by apt-get

--- .circleci/install_yarn.sh
+++ .circleci/install_yarn.sh
@@ -2,23 +2,17 @@
 # Choose which version of yarn you want to use
 EXPECTED_YARN_VERSION="0.16.1"
 
-function install_yarn {
-  mkdir -p .yarn
-  DOWNLOAD_URL="https://github.com/yarnpkg/yarn/releases/download/v$EXPECTED_YARN_VERSION/yarn-v$EXPECTED_YARN_VERSION.tar.gz"
-  echo "Downloading from $DOWNLOAD_URL"
-  curl -fL $DOWNLOAD_URL > .yarn/yarn.tar.gz
-  tar zxf .yarn/yarn.tar.gz  --strip-components=1 -C .yarn
-}
+## install node version 8
+curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+sudo apt-get install -y nodejs npm
 
-if [ -f .yarn/bin/yarn ]; then
-  YARN_VERSION=$(node -e 'const fs = require("fs"); console.log(JSON.parse(fs.readFileSync(".yarn/package.json")).version);')
-  if [ "$YARN_VERSION" != "$EXPECTED_YARN_VERSION" ]; then
-    echo "The yarn version is $YARN_VERSION, expected $EXPECTED_YARN_VERSION. Re-downloading"
-    rm -rf .yarn
-    install_yarn
-  fi
-else
-  echo "The file .yarn/bin/yarn does not exist, installing yarn".
-  install_yarn
-fi
+npm install -g nvm
+nvm install 8
+nvm alias default 8
+nvm use default
+npm install -g yarn
 
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+
+sudo apt-get update && sudo apt-get install yarn
