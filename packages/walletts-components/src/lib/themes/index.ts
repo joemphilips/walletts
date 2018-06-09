2356d5c6037de15a4d071f986ae88825cf24ad26
joemphilips <joemphilips@gmail.com>
2018-06-10 06:43:07 +0900

    remove baseUrl in components tsconfig

--- packages/walletts-components/src/lib/themes/index.ts
+++ packages/walletts-components/src/lib/themes/index.ts
@@ -7,3 +7,8 @@ export const paneBase: TS.types.NestedCSSProperties = {
   filter: 'drop-shadow(2px 2px 2px rgba(0,0,0,0.6))',
   textAlign: 'center' as 'center'
 };
+
+export const sidebarPaneBase = TS.style(paneBase, {
+  alignItems: 'center',
+  justifyContent: 'center'
+});
