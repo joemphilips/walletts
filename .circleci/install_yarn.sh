cd0f06579c5260aa1ea889ce9a8d4741f913d2a5
joemphilips <joemphilips@gmail.com>
2018-06-09 21:28:18 +0900

    stop using nvm

--- .circleci/install_yarn.sh
+++ .circleci/install_yarn.sh
@@ -5,14 +5,7 @@ set -x
 
 ## install npm
 curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
-sudo apt-get update && sudo apt-get install -y nodejs npm
-sudo npm install npm -g
-
-npm install -g nvm
-nvm install 8
-nvm alias default 8
-nvm use default
-npm install -g yarn
+sudo apt-get update && sudo apt-get install -y nodejs npm build-essential yarn
 
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
