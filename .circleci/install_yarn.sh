869168c5ef078aad4f9b2b8209f0e2f4c2e61223
joemphilips <joemphilips@gmail.com>
2018-06-09 21:45:17 +0900

    isntall nvm without using npm

--- .circleci/install_yarn.sh
+++ .circleci/install_yarn.sh
@@ -4,8 +4,11 @@ EXPECTED_YARN_VERSION="0.16.1"
 set -x
 
 ## install npm
-npm install -g npm
-npm install -g nvm
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
+
+export NVM_DIR="/opt/circleci/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm + nvm install 8
+
 nvm install 8
 nvm alias default 8
 nvm use 8
