07edd1ede1cd57946ef2a878da620f6380b597b1
joemphilips <joemphilips@gmail.com>
2018-06-13 15:09:13 +0900

    chore(meta): publish new version with \\`yarn bump\\`
    do not automate publishing right now, so instead it publishes to
     git and npm with yarn bump

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    'scope-enum': [2, 'always', ["meta", "core", "blockchain-driver", "app", "components"]]
+    'scope-enum': [2, 'always', ["meta", "core", "blockchain-driver", "app", "components", "release"]]
   }
 };
