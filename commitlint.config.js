61415dcd40e38e50b21cce3b36c8e36ea5a6ce78
joemphilips <joemphilips@gmail.com>
2018-06-14 06:04:27 +0900

    chore(meta): run husky in commit-msg hook
    instead of pre-commit hook

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  extends: ['@commitlint/config-conventional'],
+  extends: ['@commitlint/config-conventional', '@commitlint/config-lerna-scopes'],
   rules: {
     'scope-enum': [2, 'always', ["meta", "core", "blockchain-driver", "app", "components", "release"]]
   }
